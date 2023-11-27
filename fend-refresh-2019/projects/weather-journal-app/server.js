65c4c45aca40fd7851676a226f1481d4ce5548ff
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2023-11-27 08:59:10 +0100

    first draft

---
+++ fend-refresh-2019/projects/weather-journal-app/server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware - Not needed with the new express version*/
+//Here we are configuring express to use body-parser as middle-ware.
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+//Callback to debug
+function listening(){
+    console.log('server running');
+    console.log(`server running on localhost: ` + port);
+};
+
+//get
+app.get('/nameYourPath', function (req, res) {
+    res.send(projectData)
+})
+
+//Post
+app.post('/nameYourPath', function (req, res) {
+    res.send('POST received')
+});
+
+const data = [];
+
+app.post('/nameYourPath', nameYourFunction);
+
+function nameYourFunction (req, res) {
+    data.push(req.body);
+};
