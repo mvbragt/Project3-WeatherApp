b42805b6bfcb61408a98e0effe07b746ebdd8e3b
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2023-12-11 16:50:52 +0100

    need to get it visual

--- fend-refresh-2019/projects/weather-journal-app/website/app.js
+++ fend-refresh-2019/projects/weather-journal-app/website/app.js
@@ -1,7 +1,9 @@
 /* Global Variables */
 const apiKey = '39e303bf51db5ac2e61500b816291446&units=imperial';
 const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=51.5606&lon=5.0919&appid=';
-
+let temp = 0;
+const zipCode = document.getElementById('zip').value;
+const feelings = document.getElementById('feelings').value;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -14,11 +16,13 @@ const getWeatherData = async (url, apiKey)=> {
     try {
         const data = await response.json();
         console.log('get weather: ', data.main.temp)
-        return data.main.temp
+        return temp = data.main.temp
     }  catch(error) {
         console.log("error", error);
     }
 }
+
+getWeatherData(openWeatherUrl, apiKey);
 
 //postData
 const postData = async(url = '', data = {}) => {
@@ -33,27 +37,23 @@ const postData = async(url = '', data = {}) => {
     });
 
     try{
-        const newData = await response.json();
-        return newData
+        return await response.json()
     }catch(error) {
         console.log("error:", error);
     }
 }
 
+//handle on click
 function handleClick(){
-    const zipCode = document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
-    const date = document.getElementById('date').innerHTML = newDate;
-    getWeatherData(openWeatherUrl, apiKey);
-    postData('/weatherJournal', {temp, date, zipCode, feelings});
+    postData('/weatherJournal', {newDate, zipCode, feelings, temp});
 }
 
+//add eventlistner
 document.addEventListener("DOMContentLoaded", function(){
     const myButton = document.getElementById("generate")
 
     myButton.addEventListener("click", handleClick)
 });
-
 
 //get all projectData
 const retrieveData = async () =>{
@@ -62,9 +62,10 @@ const retrieveData = async () =>{
         // Transform into JSON
         const allData = await request.json()
         console.log('retrieveData: ', allData)
+
         // Write updated data to DOM elements
         document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
-        document.getElementById('content').innerHTML = allData.feel;
+        document.getElementById('content').innerHTML = allData.feelings;
         document.getElementById("date").innerHTML = allData.newDate;
     }
     catch(error) {
@@ -76,3 +77,5 @@ const retrieveData = async () =>{
         };
     }
 }
+
+retrieveData
