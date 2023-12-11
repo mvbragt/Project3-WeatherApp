15dcd9c6cc48dad0942983b4db15a40c6ed365ac
Marloes van de Wouw <marloes.van.bragt@essent.nl>
2023-12-11 12:14:09 +0100

    add http post tester

--- fend-refresh-2019/projects/weather-journal-app/server.js
+++ fend-refresh-2019/projects/weather-journal-app/server.js
@@ -1,18 +1,20 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Require Express to run server and routes
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [{}];
 
 // Start up an instance of app
 const app = express();
+const port = 3000;
 
-/* Middleware - Not needed with the new express version*/
+/* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross-origin allowance
 const cors = require('cors');
 app.use(cors());
 
@@ -20,29 +22,40 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = 3000;
+
 const server = app.listen(port, listening);
+
 
 //Callback to debug
 function listening(){
     console.log('server running');
     console.log(`server running on localhost: ` + port);
-};
+}
 
-//get
-app.get('/nameYourPath', function (req, res) {
-    res.send(projectData)
-})
-
-//Post
-app.post('/nameYourPath', function (req, res) {
-    res.send('POST received')
+app.get('/weatherJournal', function (req, response) {
+  let data = {};
+    if (projectData){
+      data = projectData[projectData.length-1]
+  }    return response.send(data)
 });
 
-const data = [];
+//Post
+function pushData(req, res) {
+    console.log('Inside pushData function in server', req.body);
+    const newDataEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+    projectData.push(newDataEntry);
+    res.send(projectData)
+}
 
-app.post('/nameYourPath', nameYourFunction);
+app.post('/weatherJournal', (req, res) =>  {
+    pushData(req,res);
+});
 
-function nameYourFunction (req, res) {
-    data.push(req.body);
-};
+
+
+
+
--- fend-refresh-2019/projects/weather-journal-app/website/app.js
+++ fend-refresh-2019/projects/weather-journal-app/website/app.js
@@ -1,5 +1,75 @@
 /* Global Variables */
+const apiKey = '39e303bf51db5ac2e61500b816291446&units=imperial';
+const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=51.5606&lon=5.0919&appid=';
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+//get weatherData
+const getWeatherData = async (url, apiKey)=> {
+    const response = await fetch(url+apiKey)
+    try {
+        const data = await response.json();
+        console.log('get weather: ', data.main.temp)
+        return data.main.temp
+    }  catch(error) {
+        console.log("error", error);
+    }
+}
+
+//postData
+const postData = async(url = '', data = {}) => {
+    console.log('postData appjs: ', data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try{
+        const newData = await response.json();
+        return newData
+    }catch(error) {
+        console.log("error:", error);
+    }
+}
+
+function handleClick(){
+    const zipCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    const date = document.getElementById('date').innerHTML = newDate;
+    const temp = document.getElementById('temp').innerHTML;
+    getWeatherData(openWeatherUrl, apiKey);
+    postData('/weatherJournal', {temp, date, zipCode, feelings});
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    const myButton = document.getElementById("generate")
+
+    myButton.addEventListener("click", handleClick)
+});
+
+
+//get all projectData
+const retrieveData = async () =>{
+    const request = await fetch('/weatherJournal');
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+        console.log('retrieveData: ', allData)
+        // Write updated data to DOM elements
+        document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+        document.getElementById('content').innerHTML = allData.feel;
+        document.getElementById("date").innerHTML = allData.newDate;
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
